f5f5bc88d541b72574d2eb03aa0101cef31f5aaa
EC2 Default User <ec2-user@ip-172-31-38-78.ec2.internal>
2020-10-16 00:15:59 +0000

    [add] define model of Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ApplicationRecord
+    
+  has_many :ordered_items, dependent: :destroy
+  belongs_to :member
+  
+  enum payment_method: { クレジットカード: 0, 銀行振り込み: 1 }
+  enum order_status: {入金待ち: 0, 入金確認中: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
   
 end
